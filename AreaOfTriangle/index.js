807b0e94a450028220d66f9cffb02c2eea496d8a
Rajan chauhan <105699999+Rajanchauhan17@users.noreply.github.com>
2025-06-29 21:57:15 +0530

    Add files via upload

---
+++ AreaOfTriangle/index.js
@@ -0,0 +1,6 @@
+function areaTriangle(length,breadth){
+let areaOfTriangle=1/2*length*breadth
+console.log(`Area of Triangle is ${areaOfTriangle}`)
+    return areaOfTriangle
+}
+areaTriangle(23,23)
