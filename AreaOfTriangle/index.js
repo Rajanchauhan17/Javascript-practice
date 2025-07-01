8f69a287b5aea961c183abfe7102ba2fa2f31dbf
Rajan chauhan <105699999+Rajanchauhan17@users.noreply.github.com>
2025-07-01 15:31:14 +0530

    Update index.js

--- AreaOfTriangle/index.js
+++ AreaOfTriangle/index.js
@@ -1,6 +1,11 @@
-function areaTriangle(length,breadth){
-let areaOfTriangle=1/2*length*breadth
-console.log(`Area of Triangle is ${areaOfTriangle}`)
-    return areaOfTriangle
+// find area of triangle by using herons formula
+function areaofT(firstside,secondside,thirdside){
+    if(firstside+secondside<=thirdside||firstside+thirdside<=secondside||secondside+thirdside<=firstside){
+        return null;
+    }
+    let side=(firstside+secondside+thirdside)/2
+    let areaofTriangle=Math.sqrt(side*(side-firstside)*(side-secondside)*(side-thirdside))
+    console.log(`Area of Triangle is ${areaofTriangle} cm`)
+    return areaofTriangle;
 }
-areaTriangle(23,23)
+ areaofT(55,32,32)
